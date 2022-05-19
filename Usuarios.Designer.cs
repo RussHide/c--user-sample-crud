de96e7fd2936a0139c675e6ff90af06fea87cc13
Fabian Hide <graadev@gmail.com>
2022-05-19 16:19:11 -0500

    first commit

---
+++ Usuarios.Designer.cs
@@ -0,0 +1,353 @@
+﻿
+namespace UserCRUD
+{
+    partial class Usuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+            this.colId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colUsuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPassword = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAgregar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEditar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEliminar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtConfirmar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chAgregar = new System.Windows.Forms.CheckBox();
+            this.chEditar = new System.Windows.Forms.CheckBox();
+            this.chEliminar = new System.Windows.Forms.CheckBox();
+            this.lblId = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(11, 361);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(171, 23);
+            this.btnEditar.TabIndex = 0;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(11, 390);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(171, 23);
+            this.btnEliminar.TabIndex = 1;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // dgvUsuarios
+            // 
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colId,
+            this.colNombre,
+            this.colUsuario,
+            this.colPassword,
+            this.colAgregar,
+            this.colEditar,
+            this.colEliminar});
+            this.dgvUsuarios.Location = new System.Drawing.Point(208, 44);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.RowHeadersVisible = false;
+            this.dgvUsuarios.RowHeadersWidth = 51;
+            this.dgvUsuarios.Size = new System.Drawing.Size(723, 369);
+            this.dgvUsuarios.TabIndex = 2;
+            this.dgvUsuarios.RowPrePaint += new System.Windows.Forms.DataGridViewRowPrePaintEventHandler(this.dgvUsuarios_RowPrePaint);
+            this.dgvUsuarios.Click += new System.EventHandler(this.dgvUsuarios_Click);
+            // 
+            // colId
+            // 
+            this.colId.HeaderText = "Id";
+            this.colId.MinimumWidth = 6;
+            this.colId.Name = "colId";
+            this.colId.Width = 60;
+            // 
+            // colNombre
+            // 
+            this.colNombre.HeaderText = "Nombre";
+            this.colNombre.MinimumWidth = 6;
+            this.colNombre.Name = "colNombre";
+            this.colNombre.Width = 150;
+            // 
+            // colUsuario
+            // 
+            this.colUsuario.HeaderText = "Usuario";
+            this.colUsuario.MinimumWidth = 6;
+            this.colUsuario.Name = "colUsuario";
+            this.colUsuario.Width = 150;
+            // 
+            // colPassword
+            // 
+            this.colPassword.HeaderText = "Contraseña";
+            this.colPassword.MinimumWidth = 6;
+            this.colPassword.Name = "colPassword";
+            this.colPassword.Width = 150;
+            // 
+            // colAgregar
+            // 
+            this.colAgregar.HeaderText = "Agregar";
+            this.colAgregar.MinimumWidth = 6;
+            this.colAgregar.Name = "colAgregar";
+            this.colAgregar.Width = 70;
+            // 
+            // colEditar
+            // 
+            this.colEditar.HeaderText = "Editar";
+            this.colEditar.MinimumWidth = 6;
+            this.colEditar.Name = "colEditar";
+            this.colEditar.Width = 70;
+            // 
+            // colEliminar
+            // 
+            this.colEliminar.HeaderText = "Eliminar";
+            this.colEliminar.MinimumWidth = 6;
+            this.colEliminar.Name = "colEliminar";
+            this.colEliminar.Width = 70;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(11, 332);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(172, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agragar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtNombre.Location = new System.Drawing.Point(12, 45);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(171, 26);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtUsuario.Location = new System.Drawing.Point(12, 98);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(171, 26);
+            this.txtUsuario.TabIndex = 6;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtContraseña.Location = new System.Drawing.Point(12, 154);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(171, 26);
+            this.txtContraseña.TabIndex = 7;
+            // 
+            // txtConfirmar
+            // 
+            this.txtConfirmar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtConfirmar.Location = new System.Drawing.Point(12, 211);
+            this.txtConfirmar.Name = "txtConfirmar";
+            this.txtConfirmar.Size = new System.Drawing.Size(171, 26);
+            this.txtConfirmar.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(12, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 20);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Contraseña";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(12, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Confirmar contraseña";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(12, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Permisos";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(204, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(213, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Tabla de usuarios y permisos";
+            // 
+            // chAgregar
+            // 
+            this.chAgregar.AutoSize = true;
+            this.chAgregar.Location = new System.Drawing.Point(12, 263);
+            this.chAgregar.Name = "chAgregar";
+            this.chAgregar.Size = new System.Drawing.Size(63, 17);
+            this.chAgregar.TabIndex = 19;
+            this.chAgregar.Text = "Agregar";
+            this.chAgregar.UseVisualStyleBackColor = true;
+            // 
+            // chEditar
+            // 
+            this.chEditar.AutoSize = true;
+            this.chEditar.Location = new System.Drawing.Point(12, 287);
+            this.chEditar.Name = "chEditar";
+            this.chEditar.Size = new System.Drawing.Size(53, 17);
+            this.chEditar.TabIndex = 20;
+            this.chEditar.Text = "Editar";
+            this.chEditar.UseVisualStyleBackColor = true;
+            // 
+            // chEliminar
+            // 
+            this.chEliminar.AutoSize = true;
+            this.chEliminar.Location = new System.Drawing.Point(11, 309);
+            this.chEliminar.Name = "chEliminar";
+            this.chEliminar.Size = new System.Drawing.Size(62, 17);
+            this.chEliminar.TabIndex = 21;
+            this.chEliminar.Text = "Eliminar";
+            this.chEliminar.UseVisualStyleBackColor = true;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Enabled = false;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 0.01F);
+            this.lblId.Location = new System.Drawing.Point(9, 7);
+            this.lblId.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(6, 2);
+            this.lblId.TabIndex = 22;
+            this.lblId.Text = "label7";
+            // 
+            // Usuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 424);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.chEliminar);
+            this.Controls.Add(this.chEditar);
+            this.Controls.Add(this.chAgregar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtConfirmar);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgvUsuarios);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Name = "Usuarios";
+            this.Text = "Usuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView dgvUsuarios;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtConfirmar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox chAgregar;
+        private System.Windows.Forms.CheckBox chEditar;
+        private System.Windows.Forms.CheckBox chEliminar;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colUsuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPassword;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAgregar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEditar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEliminar;
+    }
+}
+
---
+++ Usuarios.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using LibreriaAgenda;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace UserCRUD
+{
+    public partial class Usuarios : Form
+    {
+
+
+        public Usuarios()
+        {
+            InitializeComponent();
+            dgvUsuarios.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvUsuarios.MultiSelect = false;
+            dgvUsuarios.RowPrePaint += new DataGridViewRowPrePaintEventHandler(dgvUsuarios_RowPrePaint);
+            llenarTabla();
+        }
+
+        int selectedRow = -1;
+
+        void llenarTabla()
+        {
+            dgvUsuarios.Rows.Clear();
+            dgvUsuarios.Refresh();
+            List<LibreriaAgenda.Usuario> lst = new LibreriaAgenda.ADOUser().datosTabla();
+            foreach (var uss in lst)
+            {
+                DataGridViewRow newRow = new DataGridViewRow();
+                newRow.CreateCells(dgvUsuarios);
+                newRow.Cells[0].Value = uss.Id;
+                newRow.Cells[1].Value = uss.Name;
+                newRow.Cells[2].Value = uss.User;
+                newRow.Cells[3].Value = "****"; 
+                newRow.Cells[4].Value = uss.Add;
+                newRow.Cells[5].Value = uss.Edit;
+                newRow.Cells[6].Value = uss.Delete;
+                dgvUsuarios.Rows.Add(newRow);
+            }
+        }
+
+        void limpiarCampos()
+        {
+            txtNombre.Text = "";
+            txtUsuario.Text = "";
+            txtContraseña.Text = "";
+            txtConfirmar.Text = "";
+            chAgregar.Checked = false;
+            chEditar.Checked = false;
+            chEliminar.Checked = false;
+            lblId.Text = "";
+        }
+
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (txtConfirmar.Text == "" && txtNombre.Text == "" && txtContraseña.Text == "" && txtUsuario.Text == "")
+            {
+                MessageBox.Show("todos los campos son obligatorios");
+                return;
+            }
+            else
+            {
+                if (!chAgregar.Checked && !chEditar.Checked && !chEliminar.Checked)
+                {
+                    MessageBox.Show("seleccione almenos un permiso");
+                    return;
+
+                }
+                else
+                {
+                    LibreriaAgenda.Usuario us = new LibreriaAgenda.Usuario();
+                    us.Name = txtNombre.Text;
+                    us.User = txtUsuario.Text;
+
+                    try
+                    {
+                        if (txtContraseña.Text != txtConfirmar.Text)
+                        {
+                            MessageBox.Show("Las contraseñas no son iguales, vuelve a intentarlo");
+                            return;
+                        }
+                        else
+                        {
+                            us.Password = txtContraseña.Text;
+                        }
+                        if (chAgregar.Checked)
+                        {
+                            us.Add = "Si";
+                        }
+                        else
+                        {
+                            us.Add = "No";
+
+                        }
+                        if (chEditar.Checked)
+                        {
+                            us.Edit = "Si";
+
+                        }
+                        else
+                        {
+                            us.Edit = "No";
+                        }
+                        if (chEliminar.Checked)
+                        {
+                            us.Delete = "Si";
+                        }
+                        else
+                        {
+                            us.Delete = "No";
+                        }
+
+                        //string query = "Insert into usuario (nombre,usuario,contrasena,agregar,editar,eliminar) values('" + txtNombre.Text + "','" + txtUsuario.Text + "','" + txtContraseña.Text + "','" + agregar + "','" + editar + "','" + eliminar + "')";
+                        Console.WriteLine(us);
+                        Console.WriteLine(us.Name);
+                        Console.WriteLine(us.User);
+                        Console.WriteLine(us.Password);
+                        Console.WriteLine(us.Add);
+                        bool proc = new LibreriaAgenda.ADOUser().addUser(us);
+                        
+                        if (proc)
+                        {
+                            llenarTabla();
+                            MessageBox.Show("Se agrego correctamente");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Hubo un error al agegar el usuario");
+                        }
+                        llenarTabla();
+                    }
+                    catch (MySqlException ex)
+                    {
+                        throw ex;
+                    }
+                }
+            }
+            limpiarCampos();
+        }
+
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (selectedRow != -1)
+            {
+                if (txtConfirmar.Text == "" && txtNombre.Text == "" && txtContraseña.Text == "" && txtUsuario.Text == "")
+                {
+                    MessageBox.Show("todos los campos son obligatorios para editar");
+                    return;
+                }
+                else
+                {
+                    if (!chAgregar.Checked && !chEditar.Checked && !chEliminar.Checked)
+                    {
+                        MessageBox.Show("seleccione almenos un permiso");
+                        return;
+
+                    }
+                    else
+                    {
+                        try
+                        {
+                            LibreriaAgenda.Usuario us = new LibreriaAgenda.Usuario();
+                            us.Name = txtNombre.Text;
+                            us.User = txtUsuario.Text;
+                            if (txtContraseña.Text != txtConfirmar.Text)
+                            {
+                                MessageBox.Show("Las contraseñas no son iguales, vuelve a intentarlo");
+                                return;
+                            }
+                            else
+                            {
+                                us.Password = txtContraseña.Text;
+                            }
+                            if (chAgregar.Checked)
+                            {
+                                us.Add = "Si";
+                            }
+                            else
+                            {
+                                us.Add = "No";
+
+                            }
+                            if (chEditar.Checked)
+                            {
+                                us.Edit = "Si";
+                            }
+                            else
+                            {
+                                us.Edit = "No";
+                            }
+                            if (chEliminar.Checked)
+                            {
+                                us.Delete = "Si";
+                            }
+                            else
+                            {
+                                us.Delete = "No";
+                            }
+
+                            bool proc = new LibreriaAgenda.ADOUser().editUser(us, Convert.ToInt32(lblId.Text));
+                            MessageBox.Show("Se edito correctamente");
+                            llenarTabla();
+                        }
+                        catch (MySqlException ex)
+                        {
+                            throw ex;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debes seleccionar un registro");
+            }
+            limpiarCampos();
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (selectedRow != -1)
+            {
+                try
+                {
+                    bool proc = new LibreriaAgenda.ADOUser().deleteUser(Convert.ToInt32(lblId.Text));
+                    MessageBox.Show("Se elimino correctamente");
+                    llenarTabla();
+                }
+                catch (MySqlException ex)
+                {
+                    throw ex;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debes seleccionar un registro");
+            }
+            limpiarCampos();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void dgvUsuarios_Click(object sender, EventArgs e)
+        {
+            chAgregar.Checked = false;
+            chEditar.Checked = false;
+            chEliminar.Checked = false;
+            selectedRow = dgvUsuarios.CurrentCell.RowIndex;
+            lblId.Text = dgvUsuarios.CurrentRow.Cells[0].Value.ToString();
+            txtNombre.Text = dgvUsuarios.CurrentRow.Cells[1].Value.ToString();
+            txtUsuario.Text = dgvUsuarios.CurrentRow.Cells[2].Value.ToString();
+            txtContraseña.Text = dgvUsuarios.CurrentRow.Cells[3].Value.ToString();
+            if (dgvUsuarios.CurrentRow.Cells[4].Value.ToString() == "Si")
+            {
+                chAgregar.Checked = true;
+            }
+            if (dgvUsuarios.CurrentRow.Cells[5].Value.ToString() == "Si")
+            {
+                chEditar.Checked = true;
+            }
+            if (dgvUsuarios.CurrentRow.Cells[6].Value.ToString() == "Si")
+            {
+                chEliminar.Checked = true;
+            }
+        }
+
+        private void dgvUsuarios_RowPrePaint(object sender, DataGridViewRowPrePaintEventArgs e)
+        {
+            e.PaintParts &= ~DataGridViewPaintParts.Focus;
+        }
+    }
+}
